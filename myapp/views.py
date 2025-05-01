48feb6a807dde6964c4093dce162f920da35e8f3
sayem475 <sayemuddin471@gmail.com>
2025-05-02 00:10:27 +0600

    third commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
+def home(request):
+    context ={
+    }
+    return render(request, 'pages/home.html', context)
